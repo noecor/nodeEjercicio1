493f6a03cf354c6192fd9cd4385e1d6c9a294f2f
noecor <noeliacoronelg@hotmail.com>
2019-08-16 15:57:06 -0300

    corrijo fecha

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const resultado = require('./modulos/suma')
 
 console.log(resultado(4,6))
 
-const Date = moment().format('MMMM Do YYYY, h:mm:ss a');
+var moment = require('moment');
 
-console.log(Date())
+/*const Date = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(Date())*/
+
+console.log(moment('2019-24-03', 'YYYY-DD-MM').format('MMM Do YY'))
+let fecha =  new Date()
+console.log(fecha)
+console.log(`${fecha.getUTCFullYear()} ${fecha.getMonth()+1}`)
